f26906f21599141dee27bed29dd1002f217635fc
Yvan <fenghuijiuzhuan@163.com>
2024-01-11 23:36:16 +0800

    feat: 文件上传2

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -6,7 +6,14 @@ import {
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import {
+  AnyFilesInterceptor,
+  FileFieldsInterceptor,
+  FileInterceptor,
+  FilesInterceptor,
+} from '@nestjs/platform-express';
+import { storage } from './my-file-storage';
+import { SizePipe } from './size.pipe';
 
 @Controller('api/upload')
 export class UploadController {
@@ -22,18 +29,88 @@ export class UploadController {
     console.log('file', file);
   }
 
+  // 批量上传
   @Post('bbb')
   @UseInterceptors(
     FilesInterceptor('bbb', 3, {
       dest: 'uploads',
     }),
   )
-  // 批量上传
   uploadFiles(
     @UploadedFiles() files: Array<Express.Multer.File>,
     @Body() body,
   ) {
     console.log('body', body);
     console.log('files', files);
+  }
+
+  // 批量上传存在多个文件的字段
+  @Post('ccc')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'aaa', maxCount: 2 },
+        { name: 'bbb', maxCount: 3 },
+      ],
+      {
+        dest: 'uploads',
+      },
+    ),
+  )
+  uploadFilesManyKey(
+    @UploadedFiles()
+    files: { aaa?: Express.Multer.File[]; bbb?: Express.Multer.File[] },
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('files', files);
+  }
+
+  // 批量上传接受任意字段
+  @Post('ddd')
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      dest: 'uploads',
+    }),
+  )
+  uploadFilesAnyKey(
+    @UploadedFiles()
+    files: Express.Multer.File[],
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('files', files);
+  }
+
+  // 批量上传接受任意字段
+  @Post('eee')
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      storage: storage,
+    }),
+  )
+  uploadFilesAnyKeyStorage(
+    @UploadedFiles()
+    files: Express.Multer.File[],
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('files', files);
+  }
+
+  // 单个上传
+  // TODO 大小限制未生效
+  @Post('eee')
+  @UseInterceptors(
+    FileInterceptor('aaa', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFileSize10k(
+    @UploadedFile(SizePipe) file: Express.Multer.File,
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('file', file);
   }
 }
